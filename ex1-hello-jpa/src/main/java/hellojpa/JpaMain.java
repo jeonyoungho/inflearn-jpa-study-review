a4d738498094d760a94ec41568fba955771b2365
jeonyoungho <yhjun1000@gmail.com>
2022-06-10 18:24:42 +0900

    필드와 컬럼 매핑 실습 내용 추가

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
@@ -16,15 +15,13 @@ public class JpaMain {
 		tx.begin();
 
 		try {
-			// 영속
-			Member member = em.find(Member.class, 150L);
-			member.setName("AAAAA");
+			Member member = new Member();
+			member.setId(4L);
+			member.setUsername("C");
+			member.setRoleType(RoleType.GUEST);
 
-			em.clear(); // 영속성 컨텍스트를 통으로 초기화
+			em.persist(member);
 
-			Member member2 = em.find(Member.class, 150L);
-
-			System.out.println("==============================");
 			tx.commit();
 		} catch (Exception e) {
 			tx.rollback();
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,22 +1,22 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 public class Member {
 
 	@Id
 	private Long id;
-	private String name;
 
-	public Member() {
-	}
+	@Column(name = "name", nullable = false, unique = true)
+	private String username;
 
-	public Member(Long id, String name) {
-		this.id = id;
-		this.name = name;
-	}
+	private int age;
+
+	@Enumerated(EnumType.STRING)
+	private RoleType roleType;
 
 	public Long getId() {
 		return id;
@@ -26,11 +26,57 @@ public class Member {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getUsername() {
+		return username;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setUsername(String username) {
+		this.username = username;
 	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public RoleType getRoleType() {
+		return roleType;
+	}
+
+	public void setRoleType(RoleType roleType) {
+		this.roleType = roleType;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getTemp() {
+		return temp;
+	}
+
+	public void setTemp(int temp) {
+		this.temp = temp;
+	}
+
+	private LocalDate test1;
+	private LocalDateTime test2;
+
+	@Lob
+	private String description;
+
+	@Transient // 메모리에서만 쓰겠다
+	private int temp;
+
+	public Member() {
+	}
+
+
 }
