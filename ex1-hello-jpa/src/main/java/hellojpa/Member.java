f71a369783ced98a998f83a9f3a6efef2cbde45c
jeonyoungho <yhjun1000@gmail.com>
2022-06-23 19:27:23 +0900

    기본 키 매핑까지의 실습 내용 추가

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,22 +1,24 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Entity
+@SequenceGenerator(
+		name = "MEMBER_SEQ_GENERATOR",
+		sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+		initialValue = 1, allocationSize = 50)
 public class Member {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,
+	                generator = "MEMBER_SEQ_GENERATOR")
 	private Long id;
 
-	@Column(name = "name", nullable = false, unique = true)
+	@Column(name = "name", nullable = false)
 	private String username;
 
-	private int age;
-
-	@Enumerated(EnumType.STRING)
-	private RoleType roleType;
+	public Member() {
+	}
 
 	public Long getId() {
 		return id;
@@ -33,50 +35,4 @@ public class Member {
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public RoleType getRoleType() {
-		return roleType;
-	}
-
-	public void setRoleType(RoleType roleType) {
-		this.roleType = roleType;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public int getTemp() {
-		return temp;
-	}
-
-	public void setTemp(int temp) {
-		this.temp = temp;
-	}
-
-	private LocalDate test1;
-	private LocalDateTime test2;
-
-	@Lob
-	private String description;
-
-	@Transient // 메모리에서만 쓰겠다
-	private int temp;
-
-	public Member() {
-	}
-
-
 }
