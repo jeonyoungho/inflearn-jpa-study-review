0c71e1789a187e0521a8560070a9af4f994503ea
jeonyoungho <yhjun1000@gmail.com>
2022-06-28 09:50:15 +0900

    '단방향 연관관계' 까지의 실습 내용 추가

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,24 +15,22 @@ public class JpaMain {
 		tx.begin();
 
 		try {
-			Member member1 = new Member();
-			member1.setUsername("A");
+			Team team = new Team();
+			team.setName("TeamA");
+			em.persist(team);
 
-			Member member2 = new Member();
-			member2.setUsername("B");
+			Member member = new Member();
+			member.setUsername("member1");
+			member.setTeam(team);
+			em.persist(member);
 
-			Member member3 = new Member();
-			member3.setUsername("C");
+			em.flush();
+			em.clear();
 
-			System.out.println("============");
-			em.persist(member1); // 1, 51 (시퀀스 두번 호출)
-			em.persist(member2); // 2 MEM
-			em.persist(member3); // 3 MEM
+			Member findMember = em.find(Member.class, member.getId());
+			Team findTeam = findMember.getTeam();
+			System.out.println("findTeam = " + findTeam.getName());
 
-			System.out.println("member1.getId() = " + member1.getId());
-			System.out.println("member2.getId() = " + member2.getId());
-			System.out.println("member3.getId() = " + member3.getId());
-			System.out.println("============");
 
 			tx.commit();
 		} catch (Exception e) {
