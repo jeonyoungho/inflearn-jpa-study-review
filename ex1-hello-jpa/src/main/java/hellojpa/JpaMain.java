821c7bd1005d1bcf97986270e0590d1ac2c1c621
jeonyoungho <yhjun1000@gmail.com>
2022-07-17 20:40:05 +0900

    '상속관계 매핑' 까지의 실습 내용 추가

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -16,15 +16,20 @@ public class JpaMain {
 		tx.begin();
 
 		try {
-			Member member = new Member();
-			member.setUsername("member1");
-			em.persist(member);
 
-			Team team = new Team();
-			team.setName("TeamA");
-			team.getMembers().add(member);
-			em.persist(team);
+			Movie movie = new Movie();
+			movie.setDirector("aaa");
+			movie.setActor("bbb");
+			movie.setName("바람과 함께 사라지다");
+			movie.setPrice(10000);
 
+			em.persist(movie);
+
+			em.flush();
+			em.clear();
+
+			Item findMovie = em.find(Item.class, movie.getId());
+			System.out.println("findMovie = " + findMovie);
 
 			tx.commit();
 		} catch (Exception e) {
