b45196729d9124f6658eee1e15818b2ba0d27112
jeonyoungho <yhjun1000@gmail.com>
2022-07-24 21:42:24 +0900

    '즉시 로딩과 지연 로딩' 까지의 실습 내용 추가

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -16,15 +16,37 @@ public class JpaMain {
 		tx.begin();
 
 		try {
+			Team teamA = new Team();
+			teamA.setName("teamA");
+			em.persist(teamA);
+
+			Team teamB = new Team();
+			teamB.setName("teamB");
+			em.persist(teamB);
+
 			Member member1 = new Member();
 			member1.setUsername("member1");
 			em.persist(member1);
+			member1.setTeam(teamA);
+			em.persist(member1);
+
+			Member member2 = new Member();
+			member2.setUsername("member2");
+			em.persist(member2);
+			member2.setTeam(teamB);
+			em.persist(member2);
+
 			em.flush();
 			em.clear();
 
-			Member refMember = em.getReference(Member.class, member1.getId());
-			System.out.println("refMember = " + refMember.getClass().getName()); // Proxy
-			Hibernate.initialize(refMember); // 강제초기화
+//			Member m = em.find(Member.class, member1.getId());
+			List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class)
+					.getResultList();
+
+			for (Member member : members) {
+				System.out.println("member = " + member.getTeam().getName());
+			}
+
 
 			tx.commit();
 		} catch (Exception e) {
