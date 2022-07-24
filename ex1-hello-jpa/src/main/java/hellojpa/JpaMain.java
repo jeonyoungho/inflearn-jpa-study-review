8ca2fcf13ac726bae00fc352cd8c108203e521c4
jeonyoungho <yhjun1000@gmail.com>
2022-07-24 21:07:45 +0900

    '프록시' 까지의 실습 내용 추가

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,8 @@
 package hellojpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -17,25 +16,25 @@ public class JpaMain {
 		tx.begin();
 
 		try {
-
-			Member member = new Member();
-			member.setUsername("user1");
-			member.setCreatedBy("kim");
-			member.setCreatedDate(LocalDateTime.now());
-
-			em.persist(member);
+			Member member1 = new Member();
+			member1.setUsername("member1");
+			em.persist(member1);
 			em.flush();
 			em.clear();
+
+			Member refMember = em.getReference(Member.class, member1.getId());
+			System.out.println("refMember = " + refMember.getClass().getName()); // Proxy
+			Hibernate.initialize(refMember); // 강제초기화
 
 			tx.commit();
 		} catch (Exception e) {
 			tx.rollback();
+			e.printStackTrace();
 		} finally {
 			//close
 			em.close();
 		}
 
 		emf.close();
-
 	}
 }
