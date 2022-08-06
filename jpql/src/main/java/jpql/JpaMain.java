85360ae7ed20d30532fe42a5a701cdf7d3bdca34
jeonyoungho <yhjun1000@gmail.com>
2022-08-06 09:51:25 +0900

    'JPQL 함수' 까지의 실습 내용 추가

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -13,32 +13,20 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Member member1 = new Member();
+            member1.setUsername("관리자1");
+            member1.setAge(10);
+            em.persist(member1);
 
-            Member member = new Member();
-            member.setUsername("관리자");
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
-            member.changeTeam(team);
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            member2.setAge(20);
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-//            String query = "select " +
-//                    "case when m.age <= 10 then '학생요금'" +
-//                    "     when m.age >= 60 then '경로요금'" +
-//                    "     else '일반요금' " +
-//                    "end " +
-//                    "from Member m";
-
-//            String query = "select coalesce(m.username, '이름 없는 회원') as username " +
-//                    " from Member m";
-
-            String query = "select nullif(m.username, '관리자') as username " +
-                    " from Member m";
+            String query = "select group_concat(m.username) from Member m";
 
             List<String> result = em.createQuery(query, String.class).getResultList();
             for (String s : result) {
