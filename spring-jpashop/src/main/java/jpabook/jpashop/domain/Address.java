38f4bb5044cd49e87c5025843a8d51c2c7b5aa37
jeonyoungho <yhjun1000@gmail.com>
2022-09-09 19:19:04 +0900

    '엔티티 클래스 개발2' 까지의 실습 내용 추가

--- spring-jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ spring-jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,9 +1,16 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
+// 값 타입은 기본적으로 값이라는거 자체는 immutable 하게 설계되어야함
+// 변경이 되면 안됨, 생성할때만 생성이 되고 setter 를 아예 제공안하도록 해야함
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Embeddable
 @Getter
 public class Address {
