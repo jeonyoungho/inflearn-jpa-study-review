faa112fbb61ff757d728832a7c754ad286a36439
jeonyoungho <yhjun1000@gmail.com>
2022-10-17 09:15:59 +0900

    [실전! 스프링 부트와 JPA 활용2] '주문 조회 V3.1: 엔티티를 DTO로 변환 - 페이징과 한계 돌파' 까지의 실습 내용 추가

--- spring-jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ spring-jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -10,6 +10,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -50,9 +51,16 @@ public class OrderApiController {
 	public List<OrderDto> ordersV3() {
 		List<Order> orders = orderRepository.findAllWithItem();
 
-		for (Order order : orders) {
-			System.out.println("order ref = " + order + " id = " + order.getId());
-		}
+		List<OrderDto> result = orders.stream()
+		                              .map(o -> new OrderDto(o))
+		                              .collect(Collectors.toList());
+		return result;
+	}
+
+	@GetMapping("/api/v3.1/orders") // 컬렉션 페치 조인시 페이징을 적용하는 가장 best practice (default_batch_fetch_size 옵션 활용)
+	public List<OrderDto> ordersV3_page(@RequestParam(value = "offset", defaultValue = "0") int offset,
+	                                    @RequestParam(value = "limit", defaultValue = "100") int limit) {
+		List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
 
 		List<OrderDto> result = orders.stream()
 		                              .map(o -> new OrderDto(o))
