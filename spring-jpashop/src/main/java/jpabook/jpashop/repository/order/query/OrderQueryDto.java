e39e2f8037e0c9c81d6a9ae3f945e7a610dfb82f
jeonyoungho <yhjun1000@gmail.com>
2022-10-24 07:15:17 +0900

    [실전! 스프링 부트와 JPA 활용2] '주문 조회 V6: JPA에서 DTO로 직접 조회, 플랫 데이터 최적화' 까지의 실습 내용 추가

--- spring-jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ spring-jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.repository.order.query;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,7 +12,9 @@ import java.util.List;
  * Created by jyh1004 on 2022-10-17
  */
 
+
 @Data
+@EqualsAndHashCode(of = "orderId")
 public class OrderQueryDto {
 
 	private Long orderId;
@@ -31,6 +34,19 @@ public class OrderQueryDto {
 		this.orderDate = orderDate;
 		this.orderStatus = orderStatus;
 		this.address = address;
+	}
+
+	public OrderQueryDto(Long orderId,
+	                     String name,
+	                     LocalDateTime orderDate,
+	                     OrderStatus orderStatus,
+	                     Address address, List<OrderItemQueryDto> orderItems) {
+		this.orderId = orderId;
+		this.name = name;
+		this.orderDate = orderDate;
+		this.orderStatus = orderStatus;
+		this.address = address;
+		this.orderItems = orderItems;
 	}
 }
 
