f49214d962edc53e469d2a1ad8cbc525f333898a
jeonyoungho <yhjun1000@gmail.com>
2022-10-31 09:17:38 +0900

    [실전! 스프링 데이터 JPA] "예제 도메인 모델과 동작확인" 까지의 실습 내용 추가

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,11 +1,8 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  * Created by jyh1004 on 2022-10-31
@@ -13,17 +10,36 @@ import javax.persistence.Id;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
-	@Id @GeneratedValue
+	@Id
+	@GeneratedValue
+	@Column(name = "member_id")
 	private Long id;
 	private String username;
+	private int age;
 
-	protected Member() {
-	}
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "team_id")
+	private Team team;
 
 	public Member(String username) {
 		this.username = username;
+	}
+
+	public Member(String username, int age, Team team) {
+		this.username = username;
+		this.age = age;
+		if (team != null) {
+			changeTeam(team);
+		}
+	}
+
+	public void changeTeam(Team team) {
+		this.team = team;
+		team.getMembers().add(this);
 	}
 
 }
---
+++ data-jpa/src/main/java/study/datajpa/entity/Team.java
@@ -0,0 +1,32 @@
+package study.datajpa.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jyh1004 on 2022-10-31
+ */
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "team_id")
+	private Long id;
+	private String name;
+
+	@OneToMany(mappedBy = "team")
+	private List<Member> members = new ArrayList<>();
+
+	public Team(String name) {
+		this.name = name;
+	}
+}
