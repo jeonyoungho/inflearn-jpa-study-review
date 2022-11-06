99b2ef50dc7175f3c89ec51a71a9e112d2059c41
jeonyoungho <yhjun1000@gmail.com>
2022-11-06 15:41:50 +0900

    [실전! 스프링 데이터 JPA] "공통 인터페이스 분석" 까지의 실습 내용 추가

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,9 +3,13 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 /**
  * Created by jyh1004 on 2022-10-31
  */
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsername(String username);
 }
