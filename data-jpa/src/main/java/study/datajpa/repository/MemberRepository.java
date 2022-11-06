81729ef131b63cbee3348c002d495fcda9ae12f9
jeonyoungho <yhjun1000@gmail.com>
2022-11-06 16:15:47 +0900

    [실전! 스프링 데이터 JPA] "JPA NamedQuery" 까지의 실습 내용 추가

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -14,4 +16,17 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
     List<Member> findTop3HelloBy();
+
+    //  1. 해당 메서드명으로 엔티티명.메서드명 의 이름을 가진 네임드쿼리를 먼저 찾음,
+    //  2. 네임드쿼리가 없으면 그때 메서드명으로 쿼리를 생성하는 기능을 수행
+
+    // == 실무에서 잘 안씀 ==
+    // 1. 엔티티에 쿼리 있는 것도 너무 안좋고, 엔티티랑 레포지토리랑 왔다갔다 해야됨
+    // 2. 레포지토리 메서드에 바로 쿼리를 작성하는 기능이 너무 막강함
+
+    // * 네임드 쿼리의 장점 -> 애플리케이션 로딩 시점에 쿼리를 파싱해서 오류를 발견할 수 있음
+//    @Query(name = "Member.findByUsername")
+    List<Member> findByUsername(@Param("username") String username);
+
+
 }
