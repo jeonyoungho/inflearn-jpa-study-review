0eab3d35996b1eb532b6680a8e34296390e72d98
jeonyoungho <yhjun1000@gmail.com>
2023-02-12 18:16:47 +0900

    [실전! Querydsl] "동적 쿼리와 성능 최적화 조회 - Builder 사용" 까지의 실습 내용 추가

--- querydsl/src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -1,14 +1,21 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QTeam;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
 import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
 
 @Repository
 //@RequiredArgsConstructor
@@ -60,5 +67,34 @@ public class MemberJpaRepository {
                 .where(member.username.eq(username))
 //                .where(member.username222.eq(username)) // 컴파일 시점에 오류가 나기에 빌드 자체가 안되는게 장점이다.
                 .fetch();
+    }
+
+    public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition) {
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (StringUtils.hasText(condition.getUsername())) {
+            builder.and(member.username.eq(condition.getUsername()));
+        }
+        if (StringUtils.hasText(condition.getTeamName())) {
+            builder.and(team.name.eq(condition.getTeamName()));
+        }
+        if (condition.getAgeGoe() != null) {
+            builder.and(member.age.goe(condition.getAgeGoe()));
+        }
+        if (condition.getAgeLoe() != null) {
+            builder.and(member.age.loe(condition.getAgeLoe()));
+        }
+
+        return queryFactory
+            .select(new QMemberTeamDto(
+                    member.id.as("memberId"),
+                    member.username,
+                    member.age,
+                    team.id.as("teamId"),
+                    team.name.as("teamName")))
+            .from(member)
+            .leftJoin(member.team, team)
+            .where(builder)
+            .fetch();
     }
 }
--- querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -1,11 +1,15 @@
 package study.querydsl.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 
@@ -39,6 +43,39 @@ class MemberJpaRepositoryTest {
 //        List<Member> result2 = memberJpaRepository.findByUsername("member1");
         List<Member> result2 = memberJpaRepository.findByUsername_Querydsl("member1");
         assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    void searchTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(20);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+        /**
+         * 아무 조건도 들어가지 않게 되고 만약 데이터가 3만개가 있다하면 3만개를 다 퍼올리게 된다.
+         * 그래서 이런 동적쿼리짤땐 왠만하면 기본 조건이 있는게 좋고, limit 라도 있는게 좋다.
+         * 가급적 이런건 페이징 쿼리가 같이 들어가줘야 된다.
+         */
+
+        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+
+        assertThat(result).extracting("username").containsExactly("member3", "member4");
     }
 
 
