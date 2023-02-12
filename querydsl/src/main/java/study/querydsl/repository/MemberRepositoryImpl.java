341ea261b88e586806a6ca98f4646587825e81af
jeonyoungho <yhjun1000@gmail.com>
2023-02-12 20:42:25 +0900

    [실전! Querydsl] "리포지토리 지원 - QuerydslRepositorySupport" 까지의 실습 내용 추가

--- querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -2,11 +2,13 @@ package study.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.util.StringUtils;
 import study.querydsl.dto.MemberSearchCondition;
@@ -22,16 +24,55 @@ import static study.querydsl.entity.QTeam.team;
 
 // 구현체 클래스명은 스프링 Data Jpa 인터페이스명 + Impl 로 작명해줘야함!
 // @Repository 어노테이션 별도로 필요없다. 스프링 bean 으로 등록하고 자동으로 해준다.
-public class MemberRepositoryImpl implements MemberRepositoryCustom {
+public class MemberRepositoryImpl extends QuerydslRepositorySupport implements MemberRepositoryCustom {
+    /**
+     * Creates a new {@link QuerydslRepositorySupport} instance for the given domain type.
+     *
+     * @param domainClass must not be {@literal null}.
+     */
+//    public MemberRepositoryImpl(Class<?> domainClass) {
+//        super(domainClass);
+//    }
 
     private final JPAQueryFactory queryFactory;
+//
+//    public MemberRepositoryImpl(EntityManager em) {
+//        this.queryFactory = new JPAQueryFactory(em);
+//    }
+
+//    public MemberRepositoryImpl() {
+//        super(Member.class);
+//    }
 
     public MemberRepositoryImpl(EntityManager em) {
+        super(Member.class);
         this.queryFactory = new JPAQueryFactory(em);
     }
 
+
+
     @Override
     public List<MemberTeamDto> search(MemberSearchCondition condition) {
+
+        EntityManager entityManager = getEntityManager();
+
+        // querydsl 3버전은 from 부터 시작을 했고 그떄 QuerydslSupporter 가 만들어졌기에 어쩔수없이 from 부터 시작하게 된다.
+        List<MemberTeamDto> result = from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")))
+                .fetch();
+
         return queryFactory
                 .select(new QMemberTeamDto(
                         member.id.as("memberId"),
@@ -76,6 +117,47 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
         return new PageImpl<>(content, pageable, total);
     }
+
+    public Page<MemberTeamDto> searchPageSimple2(MemberSearchCondition condition, Pageable pageable) {
+        JPQLQuery<MemberTeamDto> jpqQuery = from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")));
+//                .offset(pageable.getOffset())
+//                .limit(pageable.getPageSize())
+
+        /**
+         * 장점
+         * - getQuerydsl().applyPagination() 스프링 데이터가 제공하는 페이징을 Querydsl로 편리하게 변환 가능(단! Sort는 오류발생)
+         * - from() 으로 시작 가능(최근에는 QueryFactory를 사용해서 select() 로 시작하는 것이 더 명시적) EntityManager 제공
+         *
+         * 한계
+         * - Querydsl 3.x 버전을 대상으로 만듬
+         * - Querydsl 4.x에 나온 JPAQueryFactory로 시작할 수 없음
+         * - select로 시작할 수 없음 (from으로 시작해야함) QueryFactory 를 제공하지 않음
+         * - 스프링 데이터 Sort 기능이 정상 동작하지 않음
+         */
+        JPQLQuery<MemberTeamDto> query = getQuerydsl().applyPagination(pageable, jpqQuery);
+
+        QueryResults<MemberTeamDto> results = query.fetchResults();
+
+
+        List<MemberTeamDto> content = results.getResults();
+        long total = results.getTotal();
+
+        return new PageImpl<>(content, pageable, total);
+    }
+
 
     @Override
     public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
